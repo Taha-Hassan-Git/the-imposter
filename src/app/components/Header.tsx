ca070ce6048b8e65d21a5ecdacd967dff2f4c62e
Taha <98838967+Taha-Hassan-Git@users.noreply.github.com>
2025-01-16 15:46:01 +0000

    home page boilerplate

---
+++ src/app/components/Header.tsx
@@ -0,0 +1,9 @@
+export default function Header() {
+  return (
+    <header className="w-full self-start p-5 bg-yellow-300">
+      <h1 className="text-2xl font-medium text-gray-800 rounded-t-lg text-center">
+        The Imposter
+      </h1>
+    </header>
+  );
+}
