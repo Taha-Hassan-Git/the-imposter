d17a4d6fc78a1839324503edcebcc169a34bc464
Taha <98838967+Taha-Hassan-Git@users.noreply.github.com>
2025-01-27 16:55:14 +0000

    joining and leaving

--- src/app/components/WaitingScreen.tsx
+++ src/app/components/WaitingScreen.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { GameInfo, Player, useGameState } from "../hooks/useGameState";
 
 export function WaitingScreen({
@@ -8,6 +8,13 @@ export function WaitingScreen({
   gameState: GameInfo;
   self: string;
 }) {
+  const { dispatch } = useGameState();
+  useEffect(() => {
+    dispatch({ type: "player-joined", payload: { name: self } });
+    return () => {
+      dispatch({ type: "player-left", payload: { name: self } });
+    };
+  }, [dispatch, self]);
   return (
     <div className="flex flex-col gap-5 p-5 items-center">
       <div className="bg-white rounded-lg shadow-md p-8 min-w-[360px]">
