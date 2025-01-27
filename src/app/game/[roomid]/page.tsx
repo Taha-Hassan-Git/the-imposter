d015fb9bc49608dbeb5deb82880beeac4f5f8a5f
Taha <98838967+Taha-Hassan-Git@users.noreply.github.com>
2025-01-27 16:44:09 +0000

    more log

--- src/app/game/[roomid]/page.tsx
+++ src/app/game/[roomid]/page.tsx
@@ -6,5 +6,6 @@ export default async function Page({
   params: Promise<{ roomId: string }>;
 }) {
   const roomId = (await params).roomId;
+  console.log("page, roomid: ", roomId);
   return <GameContainer roomId={roomId} />;
 }
